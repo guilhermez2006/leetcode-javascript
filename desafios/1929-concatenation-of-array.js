97cac5d311008f5ba63db91dff8e1eef59ce1a98
Guilherme Lima <guilhermefk123@gmail.com>
2026-07-13 23:54:13 -0300

    feat: add LeetCode #1929 solution

---
+++ desafios/1929-concatenation-of-array.js
@@ -0,0 +1,13 @@
+var getConcatenation = function (nums) {
+  const ans = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+
+  return ans;
+};
